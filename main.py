24b0a1f8b18ad6899f1355ea8c0313a448d7f231
Mahedi Hassan <mahedihassan8855@gmail.com>
2021-06-04 09:42:54 +0600

    PostgreSQL Database Connection

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from sqlalchemy.sql.functions import user
+import models
+import database
+from database import engine
+from routers import authentication, blog, user
+
+
+app = FastAPI()
+
+
+models.Base.metadata.create_all(engine)
+
+get_db = database.get_db
+app.include_router(authentication.router)
+app.include_router(blog.router)
+app.include_router(user.router)
