2c8fe1e133f6aadb9d443c243ce5aa038af79181
Mohammad Mahmud Hasan <esjgroupinc2@gmail.com>
2021-06-06 18:14:08 +0000

    database.py edited online with Bitbucket

--- database.py
+++ database.py
@@ -1,9 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:5588@localhost:5432/blog"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@postgres/postgres"
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
