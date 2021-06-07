386fbc22159715fb14f6ec8edf8dc3126366b3ca
Mohammad Mahmud Hasan <esjgroupinc2@gmail.com>
2021-06-07 16:22:30 +0000

    database.py edited online with Bitbucket

--- database.py
+++ database.py
@@ -2,14 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-import os
-
-from pydantic import BaseSettings, Field
-
-
-class Settings(BaseSettings):
-    db_url: str = Field(..., env='DATABASE_URL')
-
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@postgres:5431/postgres"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
