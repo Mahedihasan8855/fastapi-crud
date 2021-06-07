fda75c5c7a541e63cf625e321c7fb364950c7e9b
Mohammad Mahmud Hasan <esjgroupinc2@gmail.com>
2021-06-07 16:27:58 +0000

    database.py edited online with Bitbucket

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@postgres:5431/postgres"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@h172.20.0.2:5431/postgres"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
