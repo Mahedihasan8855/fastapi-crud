096c8af22d5b88ec32973c1b730d9144619ba10c
Mohammad Mahmud Hasan <esjgroupinc2@gmail.com>
2021-06-07 16:40:05 +0000

    database.py edited online with Bitbucket

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@esj_backend_dev-fastapi_app-deployment_db_1/postgres"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
